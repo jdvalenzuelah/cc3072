1eb67ab95c36c8080639ae03817fb65091bff6a6
David Valenzuela <val171001@uvg.edu.gt>
2021-07-13 21:27:01 -0600

    ex 07-13

---
+++ 07-13/ip_list.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Usage ./ip_list.sh <url>
+#    eg: ./ip_list.sh uvg.edu.gt
+
+
+wget $1 -O index.html
+grep index.html -oPe "href=\"https://.*\"" | cut -d "/" -f 3 | sed "s/\".*//g" | sort -u > domains.txt
+# TODO: remove unwanted domains from domains.txt
+while read line; do host "$line"; done < domains.txt | grep -oPe "has address.*" | sed "s/has address //g" > ips.txt
